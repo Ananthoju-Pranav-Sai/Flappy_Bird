da1e035faf7b41fbc7d44fb9642cb25cb88030fd
Ananthoju-Pranav-Sai <ai20btech11004@iith.ac.in>
2021-06-09 15:35:35 +0530

    Modified main.cpp

--- Game/src/Main.cpp
+++ Game/src/Main.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    sf::CircleShape shape(200.f);
+    shape.setFillColor(sf::Color::Red);
 
     while (window.isOpen())
     {
